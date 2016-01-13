bbe456337adc745ea19ee215a9f038c7611b8668
Miriam <miriamMota@users.noreply.github.com>
2016-01-13 11:27:41 +0100

    Update installifnot.R

--- installifnot.R
+++ installifnot.R
@@ -1,25 +1,25 @@
 installBiocifnot <- function(pckgName){
-if (!(require(pckgName, character.only = TRUE))) {
-  source("http://Bioconductor.org/biocLite.R")
-  biocLite(pckgName)
+  if (!(require(pckgName, character.only = TRUE))) {
+    source("http://Bioconductor.org/biocLite.R")
+    biocLite(pckgName)
   }
 }
 # Example 
-installBiocifnot("CMA")
+# installBiocifnot("CMA")
 
 installifnot <- function(pckgName){
-if (!(require(pckgName, character.only = TRUE))) {
-  install.packages(pckgName, dep = TRUE)
+  if (!(require(pckgName, character.only = TRUE))) {
+    install.packages(pckgName, dep = TRUE)
   }
 }
 # Example 
-installifnot("xlsx")
+# installifnot("xlsx")
 
 installGitifnot <- function(pathGit, pckgName){
-if (!(require(pckgName, character.only = TRUE))) {
-  installifnot("devtools")
-  install_github(file.path(pathGit,pckgName))
+  if (!(require(pckgName, character.only = TRUE))) {
+    installifnot("devtools")
+    install_github(file.path(pathGit,pckgName))
   }
 }
 # Example 
-installGitifnot("miriammota/","mmotaF")
+# installGitifnot("miriammota/","mmotaF")
