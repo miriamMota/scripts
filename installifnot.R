7915d6d622d1b62b03620048562d96f01d2de145
Miriam <miriamMota@users.noreply.github.com>
2016-01-13 10:46:07 +0100

    Create installifnot.R
    funcion instalar paquets cran/bioc/github

---
+++ installifnot.R
@@ -0,0 +1,25 @@
+installBiocifnot <- function(pckgName){
+if (!(require(pckgName, character.only = TRUE))) {
+  source("http://Bioconductor.org/biocLite.R")
+  biocLite(pckgName)
+  }
+}
+# Example 
+installBiocifnot("CMA")
+
+installifnot <- function(pckgName){
+if (!(require(pckgName, character.only = TRUE))) {
+  install.packages(pckgName, dep = TRUE)
+  }
+}
+# Example 
+installifnot("xlsx")
+
+installGitifnot <- function(pathGit, pckgName){
+if (!(require(pckgName, character.only = TRUE))) {
+  installifnot("devtools")
+  install_github(file.path(pathGit,pckgName))
+  }
+}
+# Example 
+installGitifnot("miriammota/","mmotaF")
