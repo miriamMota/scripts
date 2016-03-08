ebb765541b6f588b116472123e2236af7ad074db
Miriam <miriamMota@users.noreply.github.com>
2016-03-08 13:39:51 +0100

    Update installifnot.R

--- installifnot.R
+++ installifnot.R
@@ -17,15 +17,14 @@ installifnot <- function(pckgName){
 # Example 
 # installifnot("xlsx")
 
-installGitifnot <- function(pathPackage, proxy = FALSE, urlproxy = "conf_www.ir.vhebron.net", portproxy = 8081){
-  if (!(require(pathPackage, character.only = TRUE))) {
-    installifnot("devtools")
+installGitifnot <- function(pathGit, pckgName, proxy = FALSE, urlproxy = "conf_www.ir.vhebron.net", portproxy = 8081){
+    if (!(require(pckgName, character.only = TRUE))) {
+     installifnot("devtools")
     # require(curl)
-    if(proxy) set_config(use_proxy(url=urlproxy, port=portproxy)) 
-    install_github(pathPackage)
-    require(pathPackage)
-    
+     if(proxy) set_config(use_proxy(url=urlproxy, port=portproxy)) 
+     install_github(file.path(pathGit,pckgName))
+     require(pckgName)
+    }
   }
-}
 # Example 
-# installGitifnot("miriammota/mmotaF")
+# installGitifnot("miriammota/","mmotaF")
