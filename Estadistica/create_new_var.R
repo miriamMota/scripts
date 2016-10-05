5886b47d5b6a24e76210875714d3981035af8226
Miriam Mota <mimifoixbcn@gmail.com>
2016-10-05 13:03:19 +0200

    documentar new_var_cat

--- Estadistica/create_new_var.R
+++ Estadistica/create_new_var.R
@@ -1,12 +1,20 @@
-
-dat <- dat_2v
-var_con <- var_inf_concom
-lev <- levels(dat[,var_con[1]])
-name_new_var <- "new_var"
-
+############################
+## Miriam Mota Foix
+## 2016.10.05
+############################
 
 
-new_var_cat <- function(dat, var_con, df_lev, name_new_var){
+## IMPORTANTE: Es importante el orden en el que se indican los niveles. 
+## Por ejemplo: si un individuo puede tener en las distintas variables positivo negativo y dudoso
+## y consideraremos que si tiene dudoso su nuevo nivel es dudoso, pero en el caso de que sea positivo 
+## cambiara de categoria, deberemos indicar el el df_lev el orden de los niveles como: negativo, dudoso y positvo
+
+## var_con vector tipo caracter con el nombre de las variables a tener en cuenta
+## dat data frame donde se encuentran dichas variables
+## df_lev dataframe de dos columnas, donde en la primera tendremos los posibles niveles de las variables y en la segunda el nuevo nivel que se le asignara a la variable que creemos
+
+new_var_cat <- function(dat, var_con, df_lev){
+  name_new_var <- "new_var"
   pos_lev <- list()
   for (i in 1:length(var_con)) {
     for (j in 1:length(df_lev[,1])) {
@@ -24,6 +32,13 @@ new_var_cat <- function(dat, var_con, df_lev, name_new_var){
 }
 
 
+## IMPORTANTE: Es importante el orden en el que se indican los niveles. 
+## Por ejemplo: si un individuo puede tener en las distintas variables positivo negativo y dudoso
+## y consideraremos que si tiene dudoso su nuevo nivel es dudoso, pero en el caso de que sea positivo 
+## cambiara de categoria, deberemos indicar el el df_lev el orden de los niveles como: negativo, dudoso y positvo
+
+
+## Ejemplo
 aa <- as.factor(c("si","si","no","si","no"))
 bb <- as.factor(c("si","una","no","si","no"))
 cc <- as.factor(c("si","una","una","si","no"))
