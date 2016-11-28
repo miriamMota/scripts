167b38370ddb169689ca6cbd05674bf7b63bc5d4
Miriam Mota <mimifoixbcn@gmail.com>
2016-11-28 13:24:32 +0100

    installgitifnot correcció d'error

--- installifnot.R
+++ installifnot.R
@@ -18,10 +18,10 @@ installifnot <- function(pckgName){
 # installifnot("xlsx")
 
 installGitifnot <- function(pathGit, pckgName, proxy = FALSE, urlproxy = "conf_www.ir.vhebron.net", portproxy = 8081, force.install = FALSE){
-    if (!(require(pckgName, character.only = TRUE))) {
+    if (!(require(pckgName, character.only = TRUE)) | force.install) {
      installifnot("devtools")
     # require(curl)
-     if (proxy) set_config(use_proxy(url=urlproxy, port = portproxy)) 
+     if (proxy) set_config(use_proxy(url = urlproxy, port = portproxy)) 
      install_github(file.path(pathGit,pckgName), force = force.install)
      require(pckgName)
     }
