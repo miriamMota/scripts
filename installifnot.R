3848d3e1cd14ed482e7fa5a2c97a5617b2ca1e7b
Miriam Mota <mimifoixbcn@gmail.com>
2016-11-28 13:29:51 +0100

    installgitifnot correcció d'error

--- installifnot.R
+++ installifnot.R
@@ -18,13 +18,13 @@ installifnot <- function(pckgName){
 # installifnot("xlsx")
 
 installGitifnot <- function(pathGit, pckgName, proxy = FALSE, urlproxy = "conf_www.ir.vhebron.net", portproxy = 8081, force.install = FALSE){
-    if (!(require(pckgName, character.only = TRUE)) | force.install) {
-     installifnot("devtools")
+  if (!(require(pckgName, character.only = TRUE)) | force.install) {
+    installifnot("devtools")
     # require(curl)
-     if (proxy) set_config(use_proxy(url = urlproxy, port = portproxy)) 
-     install_github(file.path(pathGit,pckgName), force = force.install)
-     require(pckgName)
-    }
+    if (proxy) set_config(use_proxy(url = urlproxy, port = portproxy)) 
+    install_github(file.path(pathGit,pckgName), force = force.install)
+    require(pckgName, character.only = TRUE)
   }
+}
 # Example 
 # installGitifnot("miriammota","mmotaF",force.install = TRUE)
