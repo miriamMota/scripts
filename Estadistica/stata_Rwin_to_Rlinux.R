dbe74bdd2ca70bc44cbe2ee4c1b57767cb2b06d0
Miriam Mota <mimifoixbcn@gmail.com>
2017-03-28 08:26:54 +0200

    nova funcio

---
+++ Estadistica/stata_Rwin_to_Rlinux.R
@@ -0,0 +1,12 @@
+stata_Rwin_to_Rlinux <- function(df){
+  id_var <- grep("var",names(df))
+  names(df)[id_var] <- attr(df, "var.labels")[id_var]
+  names(df) <- iconv(names(df), "latin1", "UTF-8")
+  names(df) <- gsub(" ","_",names(df))
+  return(df)
+}
+
+# dat_enc <- read.dta("dades/pri_20170327/encuesta_conductual.dta")
+# dat_1v <- stata_Rwin_to_Rlinux(dat_1v)
+# names(dat_1v) <- gsub("/","_",names(dat_1v))
+# names(dat_1v) <- gsub("-","_",names(dat_1v))
