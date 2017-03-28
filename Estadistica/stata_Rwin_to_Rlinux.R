c1568d7a4ed1a052fbdf890c7d3bb6c637a79e36
Miriam <miriamMota@users.noreply.github.com>
2017-03-28 09:58:14 +0200

    Update stata_Rwin_to_Rlinux.R

--- Estadistica/stata_Rwin_to_Rlinux.R
+++ Estadistica/stata_Rwin_to_Rlinux.R
@@ -3,10 +3,10 @@ stata_Rwin_to_Rlinux <- function(df){
   names(df)[id_var] <- attr(df, "var.labels")[id_var]
   names(df) <- iconv(names(df), "latin1", "UTF-8")
   names(df) <- gsub(" ","_",names(df))
+  names(df) <- gsub("/","_",names(df))
+  names(df) <- gsub("-","_",names(df))
   return(df)
 }
 
 # dat_enc <- read.dta("dades/pri_20170327/encuesta_conductual.dta")
 # dat_1v <- stata_Rwin_to_Rlinux(dat_1v)
-# names(dat_1v) <- gsub("/","_",names(dat_1v))
-# names(dat_1v) <- gsub("-","_",names(dat_1v))
