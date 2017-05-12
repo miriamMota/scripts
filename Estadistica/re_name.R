426e8d779e2068bbf9efe69699fa7c8fc38ed1eb
miriam <mimifoixbcn@gmail.com>
2017-05-12 13:00:21 +0200

    millora re_name

--- Estadistica/re_name.R
+++ Estadistica/re_name.R
@@ -1,6 +1,8 @@
 
-re_name <- function(dat){
-  names(dat) <- gsub(".", "_",gsub("..", ".",gsub("..", ".", names(dat),fixed = T), fixed = T),fixed = T)
-  names(dat) <- gsub("_$","",names(dat))
-  names(dat) <- gsub("^X_","",names(dat))
+re_name <- function(data){
+  names(data) <- gsub(".", "_",gsub("..", ".",gsub("..", ".", names(data),fixed = T), fixed = T),fixed = T)
+  names(data) <- gsub("_$","",names(data))
+  names(data) <- gsub("^X_","",names(data))
+  return(data)
 }
+
