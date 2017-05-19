9867f2c4d57f8e4b15f2ab959c8ca902cf46949b
miriam <mimifoixbcn@gmail.com>
2017-05-19 14:07:04 +0200

    millora funció re_name

--- Estadistica/re_name.R
+++ Estadistica/re_name.R
@@ -3,6 +3,8 @@ re_name <- function(data){
   names(data) <- gsub(".", "_",gsub("..", ".",gsub("..", ".", names(data),fixed = T), fixed = T),fixed = T)
   names(data) <- gsub("_$","",names(data))
   names(data) <- gsub("^X_","",names(data))
+  names(data) <- chartr("áéóíú", "aeoiu", names(data))
   return(data)
 }
+
 
