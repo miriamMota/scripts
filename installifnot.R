3b9d28505e1d2dc4b2be71ae79f4551440e963f9
Miriam <miriamMota@users.noreply.github.com>
2017-08-08 13:42:46 +0200

    Update installifnot.R

--- installifnot.R
+++ installifnot.R
@@ -19,7 +19,8 @@ installifnot <- function(pckgName){
 
 installGitifnot <- function(pathGit, pckgName, proxy = FALSE, urlproxy = "conf_www.ir.vhebron.net", portproxy = 8081, force.install = FALSE){
   if (!(require(pckgName, character.only = TRUE)) | force.install) {
-    installifnot("devtools")
+    require("devtools")
+    require("httr")
     # require(curl)
     if (proxy) set_config(use_proxy(url = urlproxy, port = portproxy)) 
     install_github(file.path(pathGit,pckgName), force = force.install)
